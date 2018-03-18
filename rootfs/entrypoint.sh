dcd8814f97fd995757375d6df19ca5ea674b6c53
baobaoshow <liaozhifu@gmail.com>
2018-03-18 12:00:23 +0800

    Update entrypoint.sh

--- rootfs/entrypoint.sh
+++ rootfs/entrypoint.sh
@@ -4,7 +4,7 @@
 # Configure the service:
 #------------------------------------------------------------------------------
 env sslocal -s $SERVER_ADDR -p $SERVER_PORT -k $PASSWORD \
-  -b 0.0.0.0 -l ${LOCAL_PORT:-7070} -m ${METHOD:-'rc4-md5'} \
+  -b 0.0.0.0 -l ${LOCAL_PORT:-7070} -m ${METHOD:-'chacha20'} \
   -d start
 
 env /usr/sbin/privoxy --no-daemon /etc/privoxy/config
